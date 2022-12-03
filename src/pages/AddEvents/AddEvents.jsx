d4ed1f378c5ca67e329e80ad2d5868a3b8adc9ea
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2022-12-03 13:03:45 +0530

    Added form (#3)
    * Added form
    
    * Updated form

--- src/pages/AddEvents/AddEvents.jsx
+++ src/pages/AddEvents/AddEvents.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './AddEvents.css'
 const AddEvents = () => {
   return (
-    <div>AddEvents</div>
+    <form className="upload-form">
+    <label name="acc_add" className="acc_add">Account Address</label>
+    <input name="acc_add" className="acc_add" />
+    <label name="title" className="title">Title</label>
+    <input name="title" className="title"/>
+    <label name="desc"className="desc">Description</label>
+    <input name="desc"className="desc" />
+    <label name="file" className="File">Upload a File</label>
+    <input type="file" name="file" className="upload"/>
+  </form>
   )
 }
 
