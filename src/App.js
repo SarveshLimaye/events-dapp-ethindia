d012a690ebb321bd1934a75e0ebd3a45b1f76ab5
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-04 00:48:04 +0530

    Added Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home';
-import AddEvents from './pages/AddEvents/AddEvents';
-import OrganizerProfile from './pages/OrganizerProfile/OrganizerProfile';
-import LoginPage from './pages/LoginPage/LoginPage';
-import { Nav } from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AddEvents from "./pages/AddEvents/AddEvents";
+import OrganizerProfile from "./pages/OrganizerProfile/OrganizerProfile";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import { Nav } from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav/>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-event" element={<AddEvents />} />
           <Route path="/apply-organizer" element={<LoginPage />} />
           <Route path="/organizer-profile" element={<OrganizerProfile />} />
         </Routes>
-        </Router>
+        <Footer />
+      </Router>
     </div>
   );
 }
