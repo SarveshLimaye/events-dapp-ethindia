3541c488a6d89fb98b8fbd7e8acad3233826fdd9
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-12-04 07:33:03 +0530

    Passed data in organizer pprofile page

--- src/pages/OrganizerProfile/OrganizerProfile.jsx
+++ src/pages/OrganizerProfile/OrganizerProfile.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { userInfo } from "os";
+import React from "react";
+import { useWeb3AuthContext } from "../../contexts/SocialLoginContext";
+import "./OrganizerProfile.css";
 
 const OrganizerProfile = () => {
+  const {
+    address,
+    loading: eoaLoading,
+    userInfo,
+    connect,
+    disconnect,
+    getUserInfo,
+  } = useWeb3AuthContext();
   return (
-    <div>OrganizerProfile</div>
-  )
-}
+    <div>
+      <h2>Welcome, {userInfo.name} to your Organizer Profile</h2>
+      <div className="user-info">
+        <img className="profileImg" src={userInfo.profileImage} alt="Profile" />
+        <h3>Name: {userInfo.name}</h3>
+        <h3>Email ID: {userInfo.email}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default OrganizerProfile
+export default OrganizerProfile;
